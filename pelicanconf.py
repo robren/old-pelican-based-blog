8cc3a0d6fe81d907794c1a479959c39c2e2ea80e
Robert Rennison <robrennison@gmail.com>
2016-03-30 16:32:18 -0400

    First commit, save a working draft copy. Complete with custom.css as well as pointing to using a bootstrap theme of 'readable'

---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Rob Rennison'
+SITENAME = u'Pelican Blog Experiment'
+SITEURL = ''
+THEME = '../pelican-themes/pelican-bootstrap3/'
+BOOTSTRAP_THEME = 'readable'
+PATH = 'content'
+
+# Added to customize
+# tell pelican where your custom.css file is in your content folder
+STATIC_PATHS = ['extras/custom.css']
+# tell pelican where it should copy that file to in your output folder
+EXTRA_PATH_METADATA = {
+'extras/custom.css': {'path':'static/custom.css'}
+}
+# tell the pelican-bootstrap-3 theme where to find the custom.css file in your output folder
+CUSTOM_CSS = 'static/custom.css'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', ),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
